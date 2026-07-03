5e167d4e6ce6b3df376997e43775cd6ff797c3cd
marianelacurto <marianelacurto@gmail.com>
2026-07-03 13:09:06 +0000

    actualizacion base dde datos

--- src/models.py
+++ src/models.py
@@ -1,19 +1,103 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(nullable=False)
+    firstname: Mapped[str] = mapped_column(nullable=False)
+    lastname: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+    favorites_characters: Mapped[List["FavoriteCharacter"]] = relationship(back_populates="user")
+    favorites_planets: Mapped[List["FavoritePlanet"]] = relationship(back_populates="user")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    climate: Mapped[str] = mapped_column(nullable=False)
+    gravity: Mapped[str] = mapped_column(nullable=False)
+    population: Mapped[int] = mapped_column(nullable=False)
+
+    favorites_planets: Mapped[List["FavoritePlanet"]] = relationship(back_populates="planets")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "population": self.population,
+            # do not serialize the password, its a security breach
+        }
+
+class Characters(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    hair_color: Mapped[str] = mapped_column(nullable=False)
+    eye_color: Mapped[str] = mapped_column(nullable=False)
+    birth_year: Mapped[str] = mapped_column(nullable=False)
+
+    favorites_characters: Mapped[List["FavoriteCharacter"]] = relationship(back_populates="characters")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+                # do not serialize the password, its a security breach
+        }
+
+class FavoriteCharacter(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="favorites_characters")
+    characters: Mapped["Characters"] = relationship(back_populates="favorites_characters")
 
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "character_id": self.character_id,
+            "user_id": self.user_id,
             # do not serialize the password, its a security breach
+        }
+
+class FavoritePlanet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="favorites_planets")
+    planets: Mapped["Planets"] = relationship(back_populates="favorites_planets")
+        
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_id": self.planet_id,
+            "user_id": self.user_id,
+                # do not serialize the password, its a security breach
         }
